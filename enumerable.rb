53e969ef45db92025855a3a52c9aad0623868ae7
Fezzopro <fezzopro@gmail.com>
2023-07-21 15:10:33 +0200

    Add any? method to MyEnumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,9 +1,14 @@
 module MyEnumerable
   def all?
     @list.each do |single|
-      false if yield(element) == false
+      false if yield(single) == false
     end
     true
   end
-  
+
+  def any?
+    @list.each do |single|
+      true if yield(single) == true
+    end
+  end
 end
