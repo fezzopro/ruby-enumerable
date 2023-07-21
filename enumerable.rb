26584b44d9120719aaec79dff0388aee952c7c27
Fezzopro <fezzopro@gmail.com>
2023-07-21 15:46:32 +0200

    Add filter method to MyEnumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,14 +1,23 @@
 module MyEnumerable
   def all?
-    @list.each do |single|
-      false if yield(single) == false
+    @list.each do |element|
+      return false if yield(element) == false
     end
     true
   end
 
   def any?
     @list.each do |single|
-      true if yield(single) == true
+      return true if yield(single) == true
     end
+    false
+  end
+
+  def filter
+    tmp_array = []
+    @list.each do |single|
+      tmp_array << single if yield(single)
+    end
+    tmp_array
   end
 end
